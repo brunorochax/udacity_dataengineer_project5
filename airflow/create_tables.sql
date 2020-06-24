84bba1cebd0dcdcebdbfd3b005325f9db86fd58d
Bruno Rocha <bruno.sousax@gmail.com>
2020-06-24 00:28:54 -0300

    :recycle: Refactoring create tables scripts

--- airflow/create_tables.sql
+++ airflow/create_tables.sql
@@ -1,33 +1,3 @@
-CREATE TABLE public.artists (
-	artistid varchar(256) NOT NULL,
-	name varchar(256),
-	location varchar(256),
-	lattitude numeric(18,0),
-	longitude numeric(18,0)
-);
-
-CREATE TABLE public.songplays (
-	playid varchar(32) NOT NULL,
-	start_time timestamp NOT NULL,
-	userid int4 NOT NULL,
-	"level" varchar(256),
-	songid varchar(256),
-	artistid varchar(256),
-	sessionid int4,
-	location varchar(256),
-	user_agent varchar(256),
-	CONSTRAINT songplays_pkey PRIMARY KEY (playid)
-);
-
-CREATE TABLE public.songs (
-	songid varchar(256) NOT NULL,
-	title varchar(256),
-	artistid varchar(256),
-	"year" int4,
-	duration numeric(18,0),
-	CONSTRAINT songs_pkey PRIMARY KEY (songid)
-);
-
 CREATE TABLE public.staging_events (
 	artist varchar(256),
 	auth varchar(256),
@@ -62,17 +32,46 @@ CREATE TABLE public.staging_songs (
 	"year" int4
 );
 
-CREATE TABLE public.staging_songs (
-	num_songs int4,
-	artist_id varchar(256),
-	artist_name varchar(256),
-	artist_latitude numeric(18,0),
-	artist_longitude numeric(18,0),
-	artist_location varchar(256),
-	song_id varchar(256),
+CREATE TABLE public.songplays (
+	playid varchar(32) NOT NULL,
+	start_time timestamp NOT NULL,
+	userid int4 NOT NULL,
+	"level" varchar(256),
+	songid varchar(256),
+	artistid varchar(256),
+	sessionid int4,
+	location varchar(256),
+	user_agent varchar(256),
+	CONSTRAINT songplays_pkey PRIMARY KEY (playid)
+);
+
+CREATE TABLE IF NOT EXISTS public."time" (
+	start_time timestamp NOT NULL,
+	"hour" int4,
+	"day" int4,
+	week int4,
+	"month" varchar(256),
+	"year" int4,
+	weekday varchar(256),
+	CONSTRAINT time_pkey PRIMARY KEY (start_time)
+);
+
+CREATE TABLE public.artists (
+	artistid varchar(256) NOT NULL,
+	name varchar(256),
+	location varchar(256),
+	lattitude numeric(18,0),
+	longitude numeric(18,0),
+	CONSTRAINT artist_key PRIMARY KEY (artistid)
+);
+
+CREATE TABLE public.songs (
+	songid varchar(256) NOT NULL,
 	title varchar(256),
+	artistid varchar(256),
+	"year" int4,
 	duration numeric(18,0),
-	"year" int4
+	CONSTRAINT songs_pkey PRIMARY KEY (songid)
 );
 
 CREATE TABLE public.users (
